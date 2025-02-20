5e27bc8b68ad2ccb3544838cf04bcee38021aab0
dvalinn <tiago.andre.amorim@gmail.com>
2025-02-20 13:25:09 +0000

    #d8d84f0 refactor into SSA class

--- micropython/src/main.py
+++ micropython/src/main.py
@@ -1,72 +1,15 @@
 import time
-import json
-import network
 import random
 
-from umqtt.simple import MQTTClient
-
-def import_config() -> Dict[str, Any]:
-    config = {}
-    with open('../config/config.json', 'r') as config_file:
-        config = config | json.load(config_file)
-    with open('../config/secrets.json', 'r') as secrets_file:
-        config = config | json.load(secrets_file)
-    return config
-
-def wlan_connect(ssid: str, password: str) -> None:
-    #TODO: Error handling
-    #TODO: Check if already connected
-    #TODO: Retries with timeout
-    #TODO: Check if SSID is available
-
-    wlan = network.WLAN(network.STA_IF)
-    wlan.active(True)
-    wlan.connect(ssid, password)
-
-    print("Connecting to WiFi...", end="")
-    while not wlan.isconnected():
-        time.sleep(1)
-        print(".", end="")
-    print("\nConnected! IP Address:", wlan.ifconfig()[0])
-
-def mqtt_connect(client_id: str, broker: str, port: int) -> MQTTClient | None:
-    #TODO: Improve error handling
-    #TODO: Check if already connected
-    #TODO: Retries with timeout
-
-    try:
-        client = MQTTClient(client_id, broker, port)
-        client.connect()
-        print("Connected to MQTT broker")
-        return client
-    except Exception as e:
-        print("Failed to connect to MQTT broker:", e)
-        return None
+from src.ssa import SSA
 
 def main():
-    CONFIG = import_config()
+    ssa = SSA()
+    ssa.connect("Goodbye")
 
-    WIFI_SSID = CONFIG['wifi']['ssid']
-    WIFI_PASSWORD = CONFIG['wifi']['password']
-    wlan_connect(WIFI_SSID, WIFI_PASSWORD)
-
-    MQTT_HOST = CONFIG['broker']['host']
-    MQTT_PORT = CONFIG['broker']['port']
-    SELF_ID = CONFIG['self-id']
-    UUID = SELF_ID['uuid']
-    client = mqtt_connect(UUID, MQTT_HOST, MQTT_PORT)
-    if not client:
-        return
-
-    #send message to registration topic
-    client.publish(f"register/{UUID}", json.dumps(SELF_ID), retain=True, qos=1)
-
-    # generate values on value property topic
-    BASE_TOPIC = f"{SELF_ID['model']}/{UUID}"
-    while True:
-        client.publish(f"{BASE_TOPIC}/value", f"{random.randint(0, 100)}")
+    while(True):
+        ssa.publish("value", f"{random.randint(0, 100)}")
         time.sleep(5)
-
 
 if __name__ == "__main__":
     main()
---
+++ micropython/src/ssa.py
@@ -0,0 +1,124 @@
+import os
+import json
+import time
+import network
+
+from umqtt.simple import MQTTClient
+
+class SSA: 
+    def __init__(self):
+        self.UUID: str
+        self.BASE_TOPIC: str
+        self.REGISTRATION_TOPIC: str
+
+        self.wlan: network.WLAN | None = None 
+        self.mqtt: MQTTClient | None = None
+        pass
+
+    def __load_config(self) -> dict[str, Any]:
+        CONFIG_FILE_PATH = "../config/config.json"
+        SECRETS_FILE_PATH = "../config/secrets.json"
+
+        config: Dict[str, Any] = {}
+
+        try:
+            config_file = open(CONFIG_FILE_PATH, "r")
+            config = config | json.load(config_file)
+            config_file.close()
+        except OSError as e:
+            raise Exception(f"[ERROR] config.json could not be parsed: {e}")
+
+        try:
+            secrets_file = open(SECRETS_FILE_PATH, "r")
+            config = config | json.load(secrets_file)
+            secrets_file.close()
+        except OSError as e:
+            raise Exception(f"[ERROR] secrets.json could not be parsed: {e}")
+
+        return config
+
+    def __validate_config(self, CONFIG: Dict[str, Any]):
+        try:
+            # WIFI Config
+            CONFIG["wifi"]
+            CONFIG["wifi"]["ssid"]
+            CONFIG["wifi"]["password"]
+
+            # Broker Config
+            CONFIG["broker"]
+            CONFIG["broker"]["host"]
+            CONFIG["broker"]["port"]
+
+            # Self identification config
+            CONFIG["self-id"]
+            CONFIG["self-id"]["uuid"]
+            CONFIG["self-id"]["model"]
+            CONFIG["self-id"]["version"]
+            CONFIG["self-id"]["version"]["instance"]
+            CONFIG["self-id"]["version"]["model"]
+        except Exception as e:
+            raise Exception(f"[ERROR] malformed config: Missing \"{e}\" key")
+
+        self.UUID = CONFIG["self-id"]["uuid"]
+        self.BASE_TOPIC = f"{CONFIG["self-id"]['model']}/{self.UUID}"
+        self.REGISTRATION_TOPIC = f"registration/{self.UUID}"
+
+    def __wlan_connect(self, SSID: str, PASSWORD: str):
+        #TODO: Retries with timeout
+        #TODO: Check if already connected
+        #TODO: Check if given SSID exits
+
+        self.wlan = network.WLAN(network.STA_IF)
+        self.wlan.active(True)
+        self.wlan.connect(SSID, PASSWORD)
+
+        print("[INFO] connecting to WiFi...", end="")
+        while not self.wlan.isconnected():
+            time.sleep(1)
+            print(".", end="")
+        print("\n[INFO] connected! IP Address:", self.wlan.ifconfig()[0])
+
+    def __mqtt_connect(self,
+                       client_id: str,
+                       broker: str,
+                       port: int,
+                       last_will: str | None = None):
+        #TODO: Improve error handling
+        #TODO: Check if already connected
+        #TODO: Retries with timeout
+        #TODO: Add security
+
+        self.mqtt = MQTTClient(client_id, broker, port)
+
+        if last_will is not None and len(last_will) > 0:
+            topic = f"{self.BASE_TOPIC}/last_will",
+            self.mqtt.set_last_will(topic, last_will, qos=1)
+
+        print("Attempting to connect to MQTT broker")
+        self.mqtt.connect()
+        print("Connected to MQTT broker")
+
+    def connect(self, last_will: str | None = None):
+        CONFIG = self.__load_config()
+        self.__validate_config(CONFIG)
+
+        try:
+            WIFI_SSID = CONFIG["wifi"]["ssid"]
+            WIFI_PASSWORD = CONFIG["wifi"]["password"]
+            self.__wlan_connect(WIFI_SSID, WIFI_PASSWORD)
+        except Exception as e:
+            raise Exception(f"[ERROR] wlan connection failed: {e}")
+
+        try:
+            MQTT_HOST = CONFIG['broker']['host']
+            MQTT_PORT = CONFIG['broker']['port']
+            self.__mqtt_connect(self.UUID, MQTT_HOST, MQTT_PORT)
+        except Exception as e:
+            raise Exception(f"[ERROR] MQTT broker connection failed: {e}")
+
+        #TODO: Improve registration process to be more robust
+        self.mqtt.publish(self.REGISTRATION_TOPIC,
+                          json.dumps(CONFIG["self-id"]), retain=True, qos=1)
+
+    def publish(self, subtopic:str, msg:str, qos: int = 0):
+        self.mqtt.publish(f"{self.BASE_TOPIC}/{subtopic}", msg, qos=qos)
