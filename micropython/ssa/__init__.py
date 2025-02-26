36c49197652f9697801aa3aada3031a10a37fe64
dvalinn <tiago.andre.amorim@gmail.com>
2025-02-26 11:50:37 +0000

    api simplification. SiSSA api overhaul
    simplified the ssa decorator api.
    Extended the ssa class to allow for greater flexibility.

---
+++ micropython/ssa/__init__.py
@@ -0,0 +1,2 @@
+from .core import SSA
+from .decorators import ssa_task, ssa_main
--- micropython/ssa/decorators.py
+++ micropython/ssa/decorators.py
@@ -1,11 +1,14 @@
+import time
 import asyncio
-from typing import Any, Callable, Tuple
-from ssa.core import SSA, SSA_Property
+from ssa.core import SSA
+from typing import Callable, Awaitable
 
-def ssa_property_task(property_name: str, period_ms: int, retain: bool = False, qos: int = 0):
+def ssa_task(period_ms: int = 0):
     """! Decorator to handle the publication of a property to the broker
         @param property: The property to publish
-        @param period_ms: The period in milliseconds to publish the property
+        @param period_ms: The period in milliseconds to publish the property (0 for one shot properties)
+        @param retain: Whether the property should be retained by the broker
+        @param qos: The MQTT quality of service level for the property
 
         The decorated function will be called in a loop, sleeping for the specified period
         after each call.
@@ -20,64 +23,24 @@ def ssa_property_task(property_name: str, period_ms: int, retain: bool = False, 
 
         Note: See ssa_main decorator documentation for example usage
     """
-    def decorator(func: Callable[[], Any]):
+    def decorator(func: Awaitable[[SSA], None]):
         def wrapper():
             ssa_instance = SSA()
-            if property_name not in ssa_instance.__properties:
-                ssa_instance.__properties[property_name] = SSA_Property(None)
-
             while True:
-                property: SSA_Property = ssa_instance.__properties[property_name]
-                previous = property.value
-                result = await func()
+                next_wake_time = ticks_add(time.ticks_ms(), period_ms)
 
-                if previous != result:
-                    ssa_instance.__properties[property_name].value = result
-                    ssa_instance.__publish(f"properties/{property_name}", f"{result}",
-                                           retain=retain, qos=qos)
+                await func(ssa_instance)
+                if period_ms == 0: # one shot task
+                    break
 
-                is_event, event_value = property.check_event()
-                if is_event:
-                    ssa_instance.__publish(f"events/{property.event_name}", f"{event_value}")
-
-                #TODO: improve task periodicity by taking into account the time taken to execute the loop
-                await asyncio.sleep_ms(period_ms)
-            
-        return wrapper
-    return decorator
-
-def ssa_event_task(event: str, period_ms: int, retain: bool = False, qos: int = 0):
-    """! Decorator to handle the publication of an event to the broker
-        @param event: The event to publish
-        @param period_ms: The period in milliseconds to check for event occurrence
-
-        The decorated function will be called in a loop, sleeping for the specified period
-        after each call.
-
-        The decorated function should return a tuple of two values:
-            async def func() -> Tuple[bool, Any]
-            The first value should be a boolean indicating whether the event has occurred.
-            The second value should be the result to publish to the broker in case of event occurrence.
-
-        Note: See ssa_main decorator documentation for example usage
-    """
-    def decorator(func: Callable[[], Tuple[bool, Any]]):
-        def wrapper():
-            ssa_instance = SSA()
-
-            while True:
-                trigger_event, result = await func()
-                if trigger_event:
-                    ssa_instance.__publish(f"events/{event}", f"{result}",
-                                           retain=retain, qos=qos)
-
-                #TODO: improve task periodicity by taking into account the time taken to execute the loop
-                await asyncio.sleep_ms(period_ms)
+                sleep_time = ticks_diff(next_wake_time, time.ticks_ms())
+                if sleep_time > 0:
+                    await asyncio.sleep_ms(sleep_time)
 
         return wrapper
     return decorator
 
-def ssa_main(init_func):
+def ssa_main(last_will: str = None):
     """! Decorator to create the main function of the application
         @param init_func: A function that takes an SSA instance as an argument
         and sets up the application property/event handlers and action callbacks.
@@ -96,42 +59,25 @@ def ssa_main(init_func):
         5. Start the main loop and service incoming messages and run the registered handlershandlers
 
         example usage:
-
-            @ssa_property_task("value", 2000)
-            async def example_prop_handler():
-                return random.randint(0, 100)
-
-            @ssa_event_handler("event", 2000)
-            async def example_event_handler():
-                return random.randint(0, 100) > 50, "Event occurred"
-
-            def example_action_callback(msg: str):
-                print(f"Action triggered with message: {msg}")
-
-            @ssa_main
-            def init(ssa: SSA):
-                ssa.register_handler(example_prop_handler)
-                ssa.register_handler(example_event_handler)
-                ssa.action_callback("action", example_action_callback)
-
-            if __name__ == "__main__":
-                init()
     """
-    def main_wrapper():
-        async def ssa_entry_point():
-            ssa_instance = SSA()
+    def decorator(init_func: Callable[[SSA], None]):
+        def main_wrapper():
+            async def ssa_entry_point():
+                ssa_instance = SSA()
 
-            try:
-                init_func(ssa_instance)
-            except Exception as e:
-                raise Exception(f"[ERROR] Failed to run initial setup: {e}")
+                try:
+                    init_func(ssa_instance)
+                except Exception as e:
+                    raise Exception(f"[ERROR] Failed to run initial setup: {e}")
 
-            try:
-                ssa_instance.__connect("Goodbye")
-            except Exception as e:
-                raise Exception(f"[ERROR] SSA connection failed: {e}") from e
+                try:
+                    ssa_instance.__connect(last_will)
+                except Exception as e:
+                    raise Exception(f"[ERROR] SSA connection failed: {e}") from e
 
-            await ssa_instance.__main_loop()
+                await ssa_instance.__main_loop()
 
-        asyncio.run(ssa_entry_point())
-    return main_wrapper
+            asyncio.run(ssa_entry_point())
+
+        return main_wrapper
+    return decorator
