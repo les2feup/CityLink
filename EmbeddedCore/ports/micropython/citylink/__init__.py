690386a9f7088cffd6489b982355439870892996
dvalinn <tiago.andre.amorim@gmail.com>
2025-06-08 22:40:39 +0100

    removing old code

--- EmbeddedCore/ports/micropython/citylink/__init__.py
+++
@@ -1,3 +0,0 @@
-from .core import EmbeddedCore
-
-__all__ = ["EmbeddedCore"]
--- EmbeddedCore/ports/micropython/citylink/interfaces/__init__.py
+++
@@ -1,5 +0,0 @@
-from .affordance_handler import AffordanceHandler
-from .task_scheduler import TaskScheduler
-from .serializer import Serializer
-
-__all__ = ["AffordanceHandler", "TaskScheduler", "Serializer"]
