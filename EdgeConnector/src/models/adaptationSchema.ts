26351c8a1e4fd636568e129e28296835686fa00f
dvalinn <tiago.andre.amorim@gmail.com>
2025-06-24 17:46:12 +0100

    refactor: removed old EdgeConnector package and replaced-it with citylink-edgenode submodule

--- EdgeConnector/src/models/adaptationSchema.ts
+++
@@ -1,7 +0,0 @@
-import { z } from "../../deps.ts";
-
-export const AdaptationSchema = z.object({
-  endNodeUUID: z.string().uuid(),
-  manifest: z.string().url(),
-});
-export type AdaptationSchema = z.infer<typeof AdaptationSchema>;
--- flake.nix
+++ flake.nix
@@ -34,13 +34,9 @@
           '';
 
           nativeBuildInputs = with pkgs; [
-            # helper scripts
-
             # temporarily open/close firewall for mqtt
             openfw
             closefw
-
-            rsbkb # for crc32 package
 
             nodePackages_latest.prettier # code formatter
             black # python code formatter
@@ -49,12 +45,7 @@
             micropython # micropython runtime and cross compiler
             mosquitto # mqtt broker
 
-            # edge node development
-            deno # dev tools and runtime
-
-            doxygen # documentation generator
             plantuml # uml diagram generator
-
             python312Packages.astor # For clean.py script
             python312Packages.typing-extensions
           ];
